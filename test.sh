fe41e6fa27a44646ccc745513ef1c4ccd794be40
Michael <27004473@sun.ac.za>
2025-03-15 15:40:28 +0200

    Added RR test cases

--- test.sh
+++ test.sh
@@ -21,6 +21,9 @@ arr['H']="Priority Scheduler 5 - Longer Boy"
 arr['I']="FCFS 3 - Longer Test Case"
 arr['J']="Priority Scheduler"
 arr['K']="Deadlock Detection P1->P2->P1"
+arr['L']="RR - 1"
+arr['M']="RR - 2"
+arr['N']="RR - 3"
 
 # Clean up previous build files and logs
 rm -f obj/*.o
@@ -36,7 +39,7 @@ gcc -O2 -Wall -Wno-variadic-macros -pedantic -g -fopenmp -DDEBUG_MNGR -DDEBUG_LO
 gcc -g -fopenmp -o schedule_processes obj/logger.o obj/manager.o obj/proc_gen.o obj/proc_loader.o obj/proc_parser.o  
 
 # Loop through test cases A to J
-for i in {A..J}; do
+for i in {A..N}; do
     # Use the "1" version of the file for each test case
     process1_path="$test_input_dir/process${i}1.list"
     test_desc="${arr[$i]}"
@@ -44,6 +47,8 @@ for i in {A..J}; do
     # Determine scheduler argument based on the description
     if [[ "$test_desc" == Priority* || "$test_desc" == Deadlock* ]]; then
         scheduler_arg="0"
+    elif [[ "$test_desc" == RR* ]]; then
+        scheduler_arg="1"
     else
         scheduler_arg="2"
     fi
